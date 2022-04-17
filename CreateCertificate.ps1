ec893e4bbb0cfc9ce1c7f9188dbd67a57bf6d863
Adnan Riaz Gondal <gonadn@gmail.com>
2022-04-17 18:34:04 +0200

    Update CreateCertificate.ps1

--- CreateCertificate.ps1
+++ CreateCertificate.ps1
@@ -1,4 +1,4 @@
-﻿$cert = New-SelfSignedCertificate -Subject "CN=BaneNorMBA" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
-Export-Certificate -Cert $cert -FilePath "C:\adnan cert\BaneNorMBADev.cer"
-$mypwd = ConvertTo-SecureString -String "BnMBA2021!" -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath "C:\BaneNorMBADev.pfx" -Password $mypwd
+﻿$cert = New-SelfSignedCertificate -Subject "CN=xxxxxx" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+Export-Certificate -Cert $cert -FilePath "C:\CertName.cer"
+$mypwd = ConvertTo-SecureString -String "password" -Force -AsPlainText
+Export-PfxCertificate -Cert $cert -FilePath "C:\CertName.pfx" -Password $mypwd
